bed660834761931e92da69d3b3c5861ef0e23bf6
Laura Fiig <Laura Fiig>
2016-11-09 22:42:43 -0600

    html content outline added styling added skeleton added to js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+//set initial variables
+//correct, incorrect, unanswered
+var correct = 0
+var incorrect = 0
+var blank = 10 - (correct + incorrect)
+
+//question and answer object
+//var answers = 
+//question array of choices
+//var 
+//etc.
+
+//$(document).ready(function() {
+
+//});
+
+//skeleton
+
+//onclick start button
+//hide start button or empty row 2
+//start timer and populate
+//populate questions and answers
+//from arrays?? push to html
+//how to create radio buttons?? list?
+//use outlined html to setup classes/id's
+//will be unpopulated at start
+
+//onclick answer button
+//return answer, compare to answer in object
+//if match ++ correct
+//else ++ incorrect
+//blank = 10 - correct - incorrect
+
+//done button
+//or times up
+//hide/depopulate questions and timer
+//hide or empty button
+//done/timesup message
+//push scores to html
+
